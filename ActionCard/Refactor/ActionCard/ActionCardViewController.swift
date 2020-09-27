32890f914a13fc1887ea67fd0200995b841d3f2b
Hugo Flores Perez <hugofloresperez@Hugos-MacBook-Pro.local>
2020-09-27 18:34:51 -0400

    Inital refactor files

---
+++ ActionCard/Refactor/ActionCard/ActionCardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ActionCardViewController.swift
+//  ActionCard
+//
+//  Created by Hugo Flores Perez on 9/27/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+import UIKit
+
+class ActionCardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ ActionCard/Refactor/VerticalSlideView/VerticalSlideView.swift
@@ -0,0 +1,13 @@
+//
+//  VerticalSlideView.swift
+//  ActionCard
+//
+//  Created by Hugo Flores Perez on 9/27/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+import UIKit
+
+class VerticalSlideView: UIView {
+
+}
