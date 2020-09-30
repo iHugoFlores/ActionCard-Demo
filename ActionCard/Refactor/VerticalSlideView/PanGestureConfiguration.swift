decc1bb69caf1c573e89f9b63f9e278a8d6d3c0d
Hugo <hperez40@MGC12YH4L9JGH6.fbpld77.ford.com>
2020-09-30 08:14:11 -0400

    Additional configuration params

--- ActionCard/Refactor/VerticalSlideView/PanGestureConfiguration.swift
+++ ActionCard/Refactor/VerticalSlideView/PanGestureConfiguration.swift
@@ -17,9 +17,29 @@ import UIKit
 public struct PanGestureConfiguration {
     public let snapThreshold: CGFloat
     public let velocityThreshold: CGFloat
+    public let draggingTolerance: CGFloat
+    public let topSnapPercentage: CGFloat
+    public let bottomSnapPercentage: CGFloat
     
-    public init(snapThreshold: CGFloat = 0.5, velocityThreshold: CGFloat = 1500) {
+    public init(
+        snapThreshold: CGFloat = 0.5,
+        velocityThreshold: CGFloat = 1500,
+        draggingTolerance: CGFloat = 20,
+        topSnapPercentage: CGFloat = 0.8,
+        bottomSnapPercentage: CGFloat = 0.2) {
         self.snapThreshold = snapThreshold < 0 ? 0 : (snapThreshold > 1 ? 1 : snapThreshold)
         self.velocityThreshold = velocityThreshold
+        self.draggingTolerance = abs(draggingTolerance)
+        self.topSnapPercentage = topSnapPercentage < 0 ? 0 : (topSnapPercentage > 1 ? 1 : topSnapPercentage)
+        self.bottomSnapPercentage = bottomSnapPercentage < 0 ? 0 : (bottomSnapPercentage > 1 ? 1 : bottomSnapPercentage)
+        
+        if bottomSnapPercentage >= topSnapPercentage { fatalError("The bottomSnapPercentage should be less than topSnapPercentage") }
+    }
+    
+    public func getFrameFor(base: CGRect, state: VerticalSlideState) -> CGRect {
+        let newHeight = state.getFrameHeight(baseHeight: base.height, configuration: self)
+        let y = base.height - newHeight
+        let h = newHeight
+        return CGRect(x: base.origin.x, y: y, width: base.width, height: h)
     }
 }
